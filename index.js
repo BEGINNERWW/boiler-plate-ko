b02dc5fd1e9d7e1e88c21264e7be47944138b219
DESKTOP-1G5JRA1\mslk <beginnerww@mslk.co.kr>
2021-06-11 10:12:34 +0900

    7강 진행중

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const express = require('express')
+import express from 'express'
 const app = express()
 const port = 5000
+import { User } from './models/User'
 
+// application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: false}));
+
+// application/json
+app.use(express.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://nodetest:han0303@cluster.mlr9q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
@@ -9,10 +15,23 @@ mongoose.connect('mongodb+srv://nodetest:han0303@cluster.mlr9q.mongodb.net/myFir
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post('/register', (req, res) => {
+    //회원 가입시 필요 정보를 client에서 가져오면
+    //정보들을 database에 입력
+
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
